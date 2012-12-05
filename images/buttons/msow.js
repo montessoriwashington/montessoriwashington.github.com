e7a8391ca2c84d391a9905084134f804ff06d1d1
Jyotirmaya Nanda <jnanda@i-a-i.com>
2012-12-05 11:34:51 -0500

    Google Calendar and cover page added

---
+++ images/buttons/msow.js
@@ -0,0 +1,33 @@
+//©Xara Ltd
+if(typeof(loc)=="undefined"||loc==""){var loc="";if(document.body&&document.body.innerHTML){var tt=document.body.innerHTML;var ml=tt.match(/["']([^'"]*)msow.js["']/i);if(ml && ml.length > 1) loc=ml[1];}}
+
+var bd=3
+document.write("<style type=\"text/css\">");
+document.write("\n<!--\n");
+document.write(".msow_menu {z-index:999;border-color:#ffffff;border-style:solid;border-width:"+bd+"px 0px "+bd+"px 0px;background-color:#d4de22;position:absolute;left:0px;top:0px;visibility:hidden;}");
+document.write(".msow_plain, a.msow_plain:link, a.msow_plain:visited{text-align:left;background-color:#d4de22;color:#000000;text-decoration:none;border-color:#ffffff;border-style:solid;border-width:0px "+bd+"px 0px "+bd+"px;padding:3px 0px 3px 0px;cursor:hand;display:block;font-size:8pt;font-family:Tahoma, Verdana, Arial, sans-serif;}");
+document.write("a.msow_plain:hover, a.msow_plain:active{background-color:#f3722b;color:#ffffff;text-decoration:none;border-color:#ffffff;border-style:solid;border-width:0px "+bd+"px 0px "+bd+"px;padding:3px 0px 3px 0px;cursor:hand;display:block;font-size:8pt;font-family:Tahoma, Verdana, Arial, sans-serif;}");
+document.write("\n-->\n");
+document.write("</style>");
+
+var fc=0xffffff;
+var bc=0xf3722b;
+if(typeof(frames)=="undefined"){var frames=0;}
+
+startMainMenu("msow_left.jpg",30,10,2,0,0)
+mainMenuItem("msow_b1",".jpg",30,76,"javascript:;","","   news   ",2,2,"msow_plain");
+mainMenuItem("msow_b2",".jpg",30,103,"javascript:;","","   programs   ",2,2,"msow_plain");
+mainMenuItem("msow_b3",".jpg",30,92,"javascript:;","","   families   ",2,2,"msow_plain");
+mainMenuItem("msow_b4",".jpg",30,73,"javascript:;","","   staff   ",2,2,"msow_plain");
+mainMenuItem("msow_b5",".jpg",30,96,"javascript:;","","   calender   ",2,2,"msow_plain");
+mainMenuItem("msow_b6",".jpg",30,113,"javascript:;","","   admissions   ",2,2,"msow_plain");
+mainMenuItem("msow_b7",".jpg",30,89,"javascript:;","","   contact   ",2,2,"msow_plain");
+mainMenuItem("msow_b8",".jpg",30,78,"javascript:;","","   home   ",2,2,"msow_plain");
+endMainMenu("msow_right.jpg",30,10)
+
+startSubmenu("msow_b1","msow_menu",101);
+submenuItem("Another Button","javascript:;","","msow_plain");
+submenuItem("Another Button 2","javascript:;","","msow_plain");
+endSubmenu("msow_b1");
+
+loc="";
---
+++ images/buttons/msow2.js
@@ -0,0 +1,33 @@
+//©Xara Ltd
+if(typeof(loc)=="undefined"||loc==""){var loc="";if(document.body&&document.body.innerHTML){var tt=document.body.innerHTML;var ml=tt.match(/["']([^'"]*)msow2.js["']/i);if(ml && ml.length > 1) loc=ml[1];}}
+
+var bd=2
+document.write("<style type=\"text/css\">");
+document.write("\n<!--\n");
+document.write(".msow2_menu {z-index:999;border-color:#ffffff;border-style:solid;border-width:"+bd+"px 0px "+bd+"px 0px;background-color:#f9955e;position:absolute;left:0px;top:0px;visibility:hidden;}");
+document.write(".msow2_plain, a.msow2_plain:link, a.msow2_plain:visited{text-align:left;background-color:#f9955e;color:#000000;text-decoration:none;border-color:#ffffff;border-style:solid;border-width:0px "+bd+"px 0px "+bd+"px;padding:2px 0px 2px 0px;cursor:hand;display:block;font-size:9pt;font-family:Arial, Helvetica, sans-serif;}");
+document.write("a.msow2_plain:hover, a.msow2_plain:active{background-color:#ffcc00;color:#000000;text-decoration:none;border-color:#ffffff;border-style:solid;border-width:0px "+bd+"px 0px "+bd+"px;padding:2px 0px 2px 0px;cursor:hand;display:block;font-size:9pt;font-family:Arial, Helvetica, sans-serif;}");
+document.write("\n-->\n");
+document.write("</style>");
+
+var fc=0x000000;
+var bc=0xffcc00;
+if(typeof(frames)=="undefined"){var frames=0;}
+
+startMainMenu("",0,0,2,0,0)
+mainMenuItem("msow2_b1",".gif",30,107,"http://msowbuzz.tumblr.com/","_blank","News",2,2,"msow2_plain");
+mainMenuItem("msow2_b2",".gif",30,107,"http://www.debbiedunlap.com/msow/programs.html","","Programs",2,2,"msow2_plain");
+mainMenuItem("msow2_b3",".gif",30,107,"http://www.debbiedunlap.com/msow/families.html","","Families",2,2,"msow2_plain");
+mainMenuItem("msow2_b4",".gif",30,107,"http://www.debbiedunlap.com/msow/staff.html","","Staff",2,2,"msow2_plain");
+mainMenuItem("msow2_b5",".gif",30,107,"http://www.debbiedunlap.com/msow/calendars.html","","Calendars",2,2,"msow2_plain");
+mainMenuItem("msow2_b6",".gif",30,107,"http://www.debbiedunlap.com/msow/admissions.html","","Admissions",2,2,"msow2_plain");
+mainMenuItem("msow2_b7",".gif",30,107,"http://www.debbiedunlap.com/msow/contact.html","","Contact",2,2,"msow2_plain");
+mainMenuItem("msow2_b8",".gif",30,107,"http://www.debbiedunlap.com/msow","","Home",2,2,"msow2_plain");
+endMainMenu("",0,0);
+
+startSubmenu("msow2_b5","msow2_menu",170);
+submenuItem("Important Dates for 2012-13","http://www.debbiedunlap.com/msow/calendar.html","","msow2_plain");
+submenuItem("Our Google Calendar","http://www.debbiedunlap.com/msow/googlecalendar.html","","msow2_plain");
+endSubmenu("msow2_b5");
+
+loc="";
